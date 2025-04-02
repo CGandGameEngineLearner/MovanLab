4cd5ed1001c026e0fab45612bed055854019222b
CGandGameEngineLearner <lifesize1@qq.com>
2025-04-02 21:42:59 +0800

    Shooter

---
+++ Source/MovanLab/Public/Character/Shooter.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MovanLab/MovanLabCharacter.h"
+#include "Shooter.generated.h"
+
+UCLASS()
+class MOVANLAB_API AShooter : public AMovanLabCharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AShooter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};

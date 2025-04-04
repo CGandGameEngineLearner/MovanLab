cbb128288f098245a94b6db282db39d4ae079fcb
CGandGameEngineLearner <lifesize1@qq.com>
2025-04-04 17:29:59 +0800

    移动

---
+++ Source/MovanLab/Public/Components/ShootComponent.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ShootComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class MOVANLAB_API UShootComponent : public UActorComponent
+{
+	GENERATED_BODY()
+private:
+	float AimPitch;
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category="Shoot")
+	void SetAimPitch(float Pitch);
+	
+	UFUNCTION(BlueprintPure, Category="Shoot")
+	float GetAimPitch()const;
+	
+public:
+	// Sets default values for this component's properties
+	UShootComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
